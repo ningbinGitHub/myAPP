146802b45f5df744b8a29006665056157c1dd885
甯彬 <ningbin_meil@163.com>
2018-05-15 17:31:03 +0800

    0

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // c2.InitShow();
+
+            /*
+             * 零散小语法
+             */
+            //进位
+            var s = 1 << 3;
+
+            /*
+             * 数组
+             */
+            int[] arrayInt = new Int32[2] { 32, 23 };
+            arrayInt[0] = 12;
+            //将数组增加一个长度
+            Array.Resize(ref arrayInt, arrayInt.Length + 1);
+
+            int[] x = new int[2];
+            int[] y = new int[2];
+            x[0] = 5; x[1] = 6;
+            x.Initialize();
+            y.Initialize();
+
+            /*
+             * 索引
+             */
+            var z = new Z();
+            var zArr = z[1];
+            var zTestModelArr = z["TestModel_Arrar_" + 49];
+        }
+    }
+    #region 类的继承
+
+    public class c1
+    {
+        public c1()
+        {
+            Console.WriteLine("Str:c1");
+        }
+        public virtual void GetString()
+        {
+            Console.WriteLine("Str:C1-GetString");
+        }
+        public void Test_New()
+        {
+            Console.WriteLine("Str:C1-Test_New");
+        }
+    }
+    public class c2 : c1
+    {
+        public c2()
+        {
+            Console.WriteLine("Str:c2");
+        }
+        public override void GetString()
+        {
+            Console.WriteLine("Str:C2-GetString");
+        }
+        public new void Test_New()
+        {
+            Console.WriteLine("Str:C2-Test_New");
+        }
+        public static void InitShow()
+        {
+            c1 c = new c2();
+            c.GetString();
+            c.Test_New();
+
+            c2 c2 = new c2();
+            c2.Test_New();
+            Console.ReadKey();
+        }
+    }
+    #endregion
+
+    #region 索引
+    public class Z
+    {
+        public Z()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr.Length - 1 - i;
+            }
+
+            for (int i = 0; i < testModelArray.Length; i++)
+            {
+                testModelArray[i] = new TestModel()
+                {
+                    Id = i,
+                    Name = "TestModel_Arrar_" + i
+                };
+            }
+        }
+
+        #region 简单int数组索引
+
+        private Int32[] arr = new Int32[101];
+
+        public Int32 this[Int32 index]
+        {
+            get
+            {
+                return arr[index];
+            }
+            set
+            {
+                arr[index] = value;
+            }
+        }
+
+        #endregion
+
+        #region 数组对象索引
+        private TestModel[] testModelArray = new TestModel[50];
+        public TestModel this[String name]
+        {
+            get
+            {
+                foreach (var item in testModelArray)
+                {
+                    if (item.Name == name)
+                    {
+                        return item;
+                    }
+                }
+
+                return null;
+            }
+        }
+
+        public class TestModel
+        {
+            public Int32 Id { get; set; }
+            public String Name { get; set; }
+        }
+        #endregion
+    }
+    #endregion
+}
---
+++ demo_01/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace demo_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var path = @"C:\MyFile\myPPT.ppt";
+            //bool ret = true;
+            //Microsoft.Office.Interop.PowerPoint.Application newApp = new Microsoft.Office.Interop.PowerPoint.Application();
+            //Microsoft.Office.Interop.PowerPoint.DocumentWindows doc = null;
+            //// 缺省参数 
+            //object Unknown = Type.Missing;
+            //try
+            //{
+
+            //    // 指定原文件和目标文件 
+            //    object Source = path;
+            //    object Target = path.Substring(0, path.LastIndexOf(".")) + ".html";
+            //    //为了保险,只读方式打开 
+            //    object readOnly = true;
+            //    // 指定另存为格式(html) 
+            //    object format = Microsoft.Office.Interop.PowerPoint.PpSaveAsFileType.ppSaveAsOpenXMLShow;
+            //    // 打开doc文件 
+            //    doc = newApp.DefaultWebOptions.Open();
+            //    // 转换格式 
+            //    doc.SaveAs(ref Target, ref format,
+            //        ref Unknown, ref Unknown, ref Unknown, ref Unknown, ref Unknown, ref Unknown, ref Unknown,
+            //        ref Unknown, ref Unknown, ref Unknown, ref Unknown, ref Unknown, ref Unknown, ref Unknown);
+            //}
+
+            var entity = new dmmusicteachingEntities();
+            var list = entity.mag_user.ToList();
+        }
+
+
+        //protected bool DocToPDF(string sourcePath, string targetPath)
+        //{
+        //    bool result = false;
+        //    PpSaveAsFileType exportFormat = PpSaveAsFileType.ppsaveas
+        //    object paramMissing = Type.Missing;
+        //    Word.ApplicationClass wordApplication = new Word.ApplicationClass();
+        //    Word.Document wordDocument = null;
+        //    try
+        //    {
+        //        object paramSourceDocPath = sourcePath;
+        //        string paramExportFilePath = targetPath;
+        //        Word.WdExportFormat paramExportFormat = exportFormat;
+        //        bool paramOpenAfterExport = false;
+        //        Word.WdExportOptimizeFor paramExportOptimizeFor = Word.WdExportOptimizeFor.wdExportOptimizeForPrint;
+        //        Word.WdExportRange paramExportRange = Word.WdExportRange.wdExportAllDocument;
+        //        int paramStartPage = 0;
+        //        int paramEndPage = 0;
+        //        Word.WdExportItem paramExportItem = Word.WdExportItem.wdExportDocumentContent;
+        //        bool paramIncludeDocProps = true;
+        //    }
+        //    catch
+        //    {
+
+        //    }
+        //}
+    }
+}
